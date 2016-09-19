01285e2ea58b7d679c7fb4211ca9c6cd7e9e056a
Andrew Casal <andrewjcasal@gmail.com>
2016-09-19 14:32:36 -0700

    fix password issue

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -876,7 +876,7 @@ passport.use(new LocalStrategy(function(email, password, done) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     var params = re.test(email) ? {email: email} : {username: email};
 
-    User.findOne(params, '+password', function(err, user) {
+    User.findOne(params, function(err, user) {
         if (err) { return done(err); }
         if (!user) { return done(null, false, { message: 'Unknown user ' + email }); }
         user.comparePassword(password, function(err, isMatch) {
